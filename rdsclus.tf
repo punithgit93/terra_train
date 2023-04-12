41db62f760dfa5155c8cb0bc1b37543da04708e4
punithgit93 <punith.s993@gmail.com>
2023-04-12 10:47:58 +0530

    deleted

--- rdsclus.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_rds_cluster" "Demo" {
-  cluster_identifier          = "demotf"
-  database_name               = "demodb"
-  manage_master_user_password = true
-  master_username             = "Punith_root"
-}
